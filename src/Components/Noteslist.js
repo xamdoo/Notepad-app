8842eaab14add8484baa10b14029b0f1b569e70a
Hamda Mouhoumed <75206869+xamdoo@users.noreply.github.com>
2023-01-03 21:45:20 +0300

    complete

--- src/Components/Noteslist.js
+++ src/Components/Noteslist.js
@@ -1,26 +1,36 @@
-import { useState } from 'react';
+
 import Notes from "./Notes";
 import AddNote from './AddNote';
+import { useState } from "react";
 
 const NotesList = ()=>{
-    const [notes, setNotes] = useState([ {
-            text: "Hi",
-            date: "12/20/2022"
-        },
-        {
-            text: "Hello",
-            date: "12/20/2022"
-        },
-        {
-            text: "Hey",
-            date: "12/20/2022"
-        },
-    ])
+
+    
+    const [notes, setNotes] = useState([]);
+
+    const handleOnAdd = (text) => {
+        const newNote = {
+                id: notes[notes.length - 1] ? notes[notes.length - 1].id + 1 : 1,
+                text: text,
+                date: new Date().toLocaleDateString()
+            };
+            setNotes([...notes, newNote]);
+        };
+
+    const handleOnDelete = (id) => {
+        setNotes(notes.filter((note) => note.id !== id));
+    };
 
     return(
         <div className="notes-list">
-            {notes.map((note)=> (<Notes text={note.text} date={note.date}/>))}
-            <AddNote />
+
+            {notes.map((note)=> (
+            <Notes 
+                id={note.id} 
+                text={note.text} 
+                date={note.date}
+                handleOnDelete={handleOnDelete}/>))};
+            <AddNote handleOnAdd={handleOnAdd}/>
         </div>
     )
 }
